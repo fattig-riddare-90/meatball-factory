a1da0e840fb0ec45623eb217359a65a089c1676b
riddaren <fredrikjohansson793@live.se>
2025-08-13 11:45:21 +0200

    Remove more 'users' related lines in settings.py to fix heroku crash

--- meatballfactory/settings.py
+++ meatballfactory/settings.py
@@ -145,5 +145,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'users.CustomUser'
